7b784157164308ded670904565a5c81b41de1104
Jesse McMahon <Jessemcmahon17@gmail.com>
2021-02-10 22:40:35 -0500

    change font on landing page

--- app/javascript/src/components/Landing/Landing.js
+++ app/javascript/src/components/Landing/Landing.js
@@ -1,7 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
+import {Link} from "react-router-dom"
 import "./Landing.css"
 
 const Landing = () => {
+    const [userEmail, setUserEmail] = useState("")
+
+    const handleChange = (e) => {
+       setUserEmail(e.target.value)
+    }
+
+    const registerUser = () => {
+        console.log(userEmail)
+    }
     return (
         <div className="body">
             <div className="container">
@@ -9,9 +19,10 @@ const Landing = () => {
                 <h3>February 15th, 9:00pm</h3>
                 <div className="input-container">
                     <label htmlFor="email-registration">Register via Email: </label>
-                    <input type="text" name="email-registration" placeholder="Enter Email Here..." />
+                    <input className="email-input" onChange={handleChange} type="text" name="email-registration" placeholder="Enter Email Here..." />
                 </div>
-                <input className="button" type="submit" value="Register"/>
+                <input onClick={registerUser} className="button" type="submit" value="Register"/>
+                <Link className="sign-up-link" to="/signup"><p>Have you not been accepted as a member?</p></Link>
             </div>
         </div>
     )
