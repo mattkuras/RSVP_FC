8a1b86abba435bf4058ce18021d688cae57853f5
matt_kuras <mattkuras@gmail.com>
2021-02-11 12:05:11 -0500

    reconfig db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_10_233109) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
