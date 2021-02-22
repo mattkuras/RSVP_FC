289b527dcc533678593581027ee08ff77fcc5f36
matt_kuras <mattkuras@gmail.com>
2021-02-22 15:07:12 -0500

    fix syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,15 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
 # SMTP settings for gmail
+
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => ENV['gmail_username']
+  :user_name            => ENV['gmail_username'],
   :password             => ENV['gmail_password'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
